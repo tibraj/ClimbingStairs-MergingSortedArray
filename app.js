790afa9da1b20a204783a6821cfbf69f6cd26e10
tibraj <tibraj94@gmail.com>
2021-01-15 15:26:50 -0500

    climbgin stairs

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 //Climbing Stairs
-
+function climbingStairs(n) {
+    if(n === 0 || n === 1) return 1;
+    let first = 1;
+    let second = 2;
+    for(let i = 3; i <= n; i++) {
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+    return second;
+}
 
 //Merging Sorted Array
