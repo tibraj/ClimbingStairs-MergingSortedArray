7598eefd30273ae88b2a66e6cbdc54ed73ad2049
tibraj <tibraj94@gmail.com>
2021-01-15 15:30:54 -0500

    merge sorted array

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ console.log(climbingStairs(3));
 
 
 //Merging Sorted Array
+
+function mergeSortedArray(nums1, m, nums2, n) {
+    m--;
+    n--;
+    let i = nums1.length - 1;
+    while(i >= 0) {
+        if(n > -1 && m > -1 && nums1[m] >= nums2[n]) {
+            nums1[i] = nums1[m];
+            nums1[m] = nums1[n];
+            m--;
+        } else if(n > -1) {
+            nums1[i] = nums2[n--];
+        }
+        i--;
+    }
+}
