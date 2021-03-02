6d53b2abba9ec736337c5f738d3346bc3be6fac8
tibraj <tibraj94@gmail.com>
2021-03-02 13:13:11 -0500

    merge sorted array third solution

--- app.js
+++ app.js
@@ -41,7 +41,23 @@ function mergeSortedArray2(nums1, m, nums2, n) {
     nums1.sort((a, b) => a - b);
 }
 
-console.log(mergeSortedArray2([1,2,3,0,0,0], 3, [2,5,6], 3));
+function mergeSortedArray3(nums1, m, nums2, n) {
+    let first = m - 1;
+    let second = n - 1;
+    for(let i = m + n - 1; i >= 0; i--) {
+        if(second < 0) {
+            break;
+        }
+        if(nums1[first] > nums2[second]) {
+            nums1[i] = nums1[first];
+            first--;
+        } else {
+            nums1[i] = nums2[second];
+            second--;
+        }
+    }
+}
+console.log(mergeSortedArray3([1,2,3,0,0,0], 3, [2,5,6], 3));
 console.log(mergeSortedArray2([1], 1, [], 0));
 
 function reverseString(s) {
